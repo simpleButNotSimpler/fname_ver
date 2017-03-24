eadc2fd194e79e6be76970627e6768c101dadca4
djimy <djimythetop@hotmail.com>
2017-03-24 14:35:24 +0800

    test enhanced

--- test.m
+++ test.m
@@ -1,5 +1,11 @@
 %% build the file names txt file
-%get the names from the folder
+%create the 'fnames.txt' file
+% list = dir('files/*.rtf');
+% fn = {list.name};
+% fn = fn';
+% fn = cell2table(fn);
+% writetable(fn,'fnames.txt', 'WriteVariableNames', false);
+
 
 %get the database and the fnames
 db = file2array('database.txt');
@@ -12,6 +18,7 @@ corr = getcorresp(fnames, db);
 %remove the value rows with score 20
 log = corr(corr(:, 3) ~= 20, :);
 
+
 %% output a log file
 %put the log in table format
 fnameFolderIndex = log(:, 1);
@@ -21,14 +28,17 @@ correctedName = char(db(fnameDBIndex, :));
 score = log(:, 3);
 t = table(fnameFolderIndex, detectedName, fnameDBIndex, correctedName, score);
 
+
 %% output the result in a text file
 writetable(t,'logFile','FileType','spreadsheet');
-writetable(t,'logFile.txt','Delimiter',' ');
+writetable(t,'logFile.txt','Delimiter','|');
+
 
 %% update the file names in the folder
 updatedNames = fnames;
 updatedNames(fnameFolderIndex, :) = db(fnameDBIndex, :);
 st = table(char(updatedNames));
 writetable(st,'updatedNames.txt', 'WriteVariableNames', false);
+
 
 %% change the file names inside the folder
